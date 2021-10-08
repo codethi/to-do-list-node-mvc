62815397413a596895404efc05d294e161e51b0a
codethi <docencia.thiago@gmail.com>
2021-10-08 19:01:13 -0300

    connect mongo atlas

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectToDb = () => {
-  mongoose.connect("mongodb://localhost:27017/todolist", {
+  mongoose.connect("mongodb+srv://root:OCof8ZSceHsAbyZN@to-do-list.wtxf9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }).then(() => {
